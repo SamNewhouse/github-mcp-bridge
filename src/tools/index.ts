9351902ca680593132b2c87172a451f92cd03ad5
Sam Newhouse <60176563+SamNewhouse@users.noreply.github.com>
2026-07-21 02:13:39 +0100

    feat: register all new tools

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 import { AppError } from "../lib/errors";
+import { addIssueCommentTool } from "./add-issue-comment";
 import { createBranchTool } from "./create-branch";
 import { createIssueTool } from "./create-issue";
 import { createPullRequestTool } from "./create-pull-request";
+import { getBranchTool } from "./get-branch";
+import { getCommitTool } from "./get-commit";
 import { getFileContentsTool } from "./get-file-contents";
 import { getIssueTool } from "./get-issue";
 import { getMultipleFilesTool } from "./get-multiple-files";
@@ -10,12 +13,16 @@ import { getPullRequestDiffTool } from "./get-pull-request-diff";
 import { getPullRequestTool } from "./get-pull-request";
 import { linkIssueToPullRequestTool } from "./link-issue-to-pull-request";
 import { listBranchesTool } from "./list-branches";
+import { listCommitsTool } from "./list-commits";
 import { listDirectoryTool } from "./list-directory";
+import { listIssueCommentsTool } from "./list-issue-comments";
 import { listIssuesTool } from "./list-issues";
 import { listOpenPullRequestsTool } from "./list-open-pull-requests";
 import { listPullRequestCommentsTool } from "./list-pull-request-comments";
 import { listPullRequestFilesTool } from "./list-pull-request-files";
 import { listRepositoriesTool } from "./list-repositories";
+import { searchCodeTool } from "./search-code";
+import { searchFilesTool } from "./search-files";
 import { updateIssueTool } from "./update-issue";
 import { updatePullRequestTool } from "./update-pull-request";
 import { upsertFileTool } from "./upsert-file";
@@ -40,6 +47,13 @@ const tools = [
   createIssueTool,
   updateIssueTool,
   linkIssueToPullRequestTool,
+  searchCodeTool,
+  searchFilesTool,
+  getCommitTool,
+  listCommitsTool,
+  getBranchTool,
+  listIssueCommentsTool,
+  addIssueCommentTool,
 ] as const;
 
 export const toolDefinitions = Object.fromEntries(
